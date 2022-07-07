d93096337f2542ce4a426a75a5da7815fa32005d
chintan-albiorix <ViShNu.ArEdLa4200@GmAiL.CoM>
2022-07-07 10:16:20 +0530

    code updated

--- client/src/Fetch.js
+++ client/src/Fetch.js
@@ -1,5 +1,6 @@
 export async function postData(route = "", data = {}, methodType) {
-  const response = await fetch(`http://localhost:3000/${route}`, {
+  const response = await fetch(`${route}`, {
+  // const response = await fetch(`http://localhost:3000/${route}`, {
     method: methodType,
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +16,8 @@ export async function postData(route = "", data = {}, methodType) {
 }
 
 export async function getData(route = "") {
-  const response = await fetch(`http://localhost:3000/${route}`, {
+  const response = await fetch(`${route}`, {
+  // const response = await fetch(`http://localhost:3000/${route}`, {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
@@ -31,7 +33,8 @@ export async function getData(route = "") {
 
 
 export async function deltetData(route = "") {
-  const response = await fetch(`http://localhost:3000/${route}`, {
+  const response = await fetch(`${route}`, {
+  // const response = await fetch(`http://localhost:3000/${route}`, {
     method:"DELETE"
   });
   if (response.ok) {
--- index.js
+++ index.js
@@ -14,13 +14,10 @@ mongoose.connect(process.env.mongoURI)
 
 app.use(express.json());
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 
-// app.use(express.static(__dirname + "/client/build"));
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
+
+app.use(express.static(__dirname + "/client/build"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
 // app.use(express.static(__dirname + "/public"));
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
